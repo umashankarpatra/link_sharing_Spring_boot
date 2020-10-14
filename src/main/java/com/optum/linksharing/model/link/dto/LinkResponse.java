66bed70205c5c4097b0d65aac48c296ca6fd49a2
Patra <uma_patra@optum.com>
2020-10-14 17:46:26 +0530

    added acutator code

--- src/main/java/com/optum/linksharing/model/link/dto/LinkResponse.java
+++ src/main/java/com/optum/linksharing/model/link/dto/LinkResponse.java
@@ -6,9 +6,6 @@ import java.util.Set;
 import com.optum.linksharing.model.Link;
 import com.optum.linksharing.model.Tag;
 
-import lombok.Data;
-
-@Data
 public class LinkResponse implements Serializable {
 
 	private static final long serialVersionUID = 1L;
